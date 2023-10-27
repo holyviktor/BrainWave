f400a5867a4d1497bd9a924dc12483390f9e8e76
Victoria Fox <83537604+holyviktor@users.noreply.github.com>
2023-10-27 16:45:09 +0300

    created DTOs for parameters of methods of Interactions Service

---
+++ BrainWave.Core/DTOs/AddCommentDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrainWave.Core.DTOs
+{
+    public class AddCommentDTO
+    {
+        public int idArticle;
+        public int idUser;
+        public string comment;
+        public AddCommentDTO(int idArticle, int isUser, string comment) {
+            this.idArticle = idArticle;
+            this.idUser = isUser;
+            this.comment = comment;
+        }
+    }
+}
---
+++ BrainWave.Core/DTOs/LikesSavingsDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrainWave.Core.DTOs
+{
+    public class LikesSavingsDTO
+    {
+        public bool status;
+        public int idArticle;
+        public int idUser;
+        public LikesSavingsDTO(bool status, int idArticle, int idUser)
+        {
+            this.status = status;
+            this.idArticle = idArticle;
+            this.idUser = idUser;
+        }
+    }
+}
